01fb5c3f6b9a7f242145791eb736a715d9b59a3e
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-25 19:50:02 +0300

    Update AppConfig.swift

--- PinholeCalc/Darkroom/AppConfig.swift
+++ PinholeCalc/Darkroom/AppConfig.swift
@@ -2,8 +2,8 @@ import Foundation
 
 enum AppConfig {
     static let plateAnchor = URL(string: "https://keitaro-zaglushka.com")!
-    static let privacyPolicyURL = URL(string: "https://hallowtommy.github.io/pinhole-camera-calc-privacy")!
-    static let supportEmail = "support@hallowtommy.app"
+    static let privacyPolicyURL = URL(string: "https://www.termsfeed.com/live/0a223bbd-a319-42ec-9822-71c32c44eda3")!
+    static let supportEmail = "cr0uz1erjas@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
